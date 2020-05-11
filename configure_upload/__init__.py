74b318f0ad316c559334b21727bf190a98b821d3
hwwilliams <20447447+hwwilliams@users.noreply.github.com>
2020-05-11 03:11:59 -0400

    upload loggers

--- configure_upload/__init__.py
+++ configure_upload/__init__.py
@@ -3,6 +3,9 @@ import logging
 botocore_auth_logger = logging.getLogger('botocore.auth')
 botocore_auth_logger.setLevel(logging.WARNING)
 
+botocore_awsrequest_logger = logging.getLogger('botocore.awsrequest')
+botocore_awsrequest_logger.setLevel(logging.WARNING)
+
 botocore_client_logger = logging.getLogger('botocore.client')
 botocore_client_logger.setLevel(logging.WARNING)
 
@@ -11,6 +14,9 @@ botocore_credentials_logger.setLevel(logging.WARNING)
 
 botocore_endpoint_logger = logging.getLogger('botocore.endpoint')
 botocore_endpoint_logger.setLevel(logging.WARNING)
+
+botocore_handlers_logger = logging.getLogger('botocore.handlers')
+botocore_handlers_logger.setLevel(logging.WARNING)
 
 botocore_hooks_logger = logging.getLogger('botocore.hooks')
 botocore_hooks_logger.setLevel(logging.WARNING)
@@ -26,6 +32,15 @@ botocore_retryhandler_logger.setLevel(logging.WARNING)
 
 botocore_utils_logger = logging.getLogger('botocore.utils')
 botocore_utils_logger.setLevel(logging.WARNING)
+
+s3transfer_futures_logger = logging.getLogger('s3transfer.futures')
+s3transfer_futures_logger.setLevel(logging.WARNING)
+
+s3transfer_tasks_logger = logging.getLogger('s3transfer.tasks')
+s3transfer_tasks_logger.setLevel(logging.WARNING)
+
+s3transfer_utils_logger = logging.getLogger('s3transfer.utils')
+s3transfer_utils_logger.setLevel(logging.WARNING)
 
 urllib3_connectionpool_logger = logging.getLogger('urllib3.connectionpool')
 urllib3_connectionpool_logger.setLevel(logging.WARNING)
