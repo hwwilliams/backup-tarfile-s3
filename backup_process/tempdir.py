34c5e7431bcf354b5afa347040edb09f1ccdb4d8
hwwilliams <20447447+hwwilliams@users.noreply.github.com>
2020-05-13 03:14:31 -0400

    clean up formatting

--- backup_process/tempdir.py
+++ backup_process/tempdir.py
@@ -9,11 +9,14 @@ logger = logging.getLogger(__name__)
 class TemporaryDirectory(object):
     def __enter__(self):
         self.name = tempfile.mkdtemp()
+
         logger.debug(
             f'Created temp directory: {json.dumps({"Directory": self.name})}')
+
         return self.name
 
     def __exit__(self, exc_type, exc_value, traceback):
         shutil.rmtree(self.name)
+
         logger.debug(
             f'Destroyed temp directory: {json.dumps({"Directory": self.name})}')
